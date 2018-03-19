16ad94ad7e0963bec9fd16fb46d834776fbccf45
Yanan <yanan@8siannetwork.com>
2018-03-19 18:51:05 +0800

    add all images, add basic item to index page

--- js/components/Index.js
+++ js/components/Index.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, PixelRatio, Image, Button} from 'react-native';
 
-class Index extends React.Component {
+class Index extends Component {
   static navigationOptions = {
-    title: 'Home',
+    title: 'TIMEMORE Labs',
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Reader/>
+        <Setting/>
+        <View>
+          <Button title="开始冲煮"/>
+        </View>
+      </View>
+    );
+  }
+}
+
+class Reader extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={require('../../images/cover.png')} />
+        <View>
+          <Text style={styles.readerTitle}>咖啡萃取量(g)</Text>
+          <Text style={styles.reader}>200</Text>
+          <Text style={styles.readerTitle}>注水总量(g)</Text>
+          <Text style={styles.reader}>200</Text>
+          <Text>归零</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+class Setting extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View>
+          <Image style={styles.settingIcon} source={require('../../images/icon_brand.png')} />
+          <Text style={styles.settingName}>咖啡豆</Text>
+          <Text style={styles.settingValue}>曼特宁</Text>
+        </View>
+
+        <View>
+          <Image style={styles.settingIcon} source={require('../../images/icon_beanweight.png')} />
+          <Text style={styles.settingName}>粉重</Text>
+          <Text style={styles.settingValue}>20g</Text>
+          <Text>读秤</Text>
+        </View>
+
+        <View>
+          <Image style={styles.settingIcon} source={require('../../images/icon_waterweight.png')} />
+          <Text style={styles.settingName}>萃取量</Text>
+          <Text style={styles.settingValue}>240g</Text>
+        </View>
+
+        <View>
+          <Image style={styles.settingIcon} source={require('../../images/icon_proportion.png')} />
+          <Text style={styles.settingName}>粉液比</Text>
+          <Text style={styles.settingValue}>1:12</Text>
+        </View>
+
+        <View>
+          <Image style={styles.settingIcon} source={require('../../images/icon_temp.png')} />
+          <Text style={styles.settingName}>温度</Text>
+          <Text style={styles.settingValue}>92℃</Text>
+        </View>
+
+        <View>
+          <Image style={styles.settingIcon} source={require('../../images/icon_grandsize.png')} />
+
+          <Text style={styles.settingName}>研磨度</Text>
+          <Text style={styles.settingValue}>3.5</Text>
+        </View>
+
+        <View>
+          <Image style={styles.settingIcon} source={require('../../images/icon_time.png')} />
+          <Text style={styles.settingName}>预计时间</Text>
+          <Text style={styles.settingValue}>02:30</Text>
+        </View>
+
+        <View>
+          <Image style={styles.settingIcon} source={require('../../images/index_btn_setting.jpg')} />
+          <Text>设置参数</Text>
+        </View>
       </View>
     );
   }
@@ -24,6 +100,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  readerTitle: {
+    color: '#5B5B5B',
+  },
+  reader: {
+    fontWeight: 'bold',
+    color: '#232323',
+  },
+  settingIcon: {
+    width: 40,
+    height: 44,
+  },
+  settingName: {
+    color: '#5B5B5B',
+  },
+  settingValue: {
+    fontWeight: 'bold',
+    color: '#232323',
+    overflow: 'hidden',
+  },
+
 });
 
 export default Index
