fe7d9e4da9e14394380695a35a99f6eb9c18dd00
Yanan <ya_nan@ivideocloud.com>
2018-03-21 14:25:14 +0800

    complete index.js, remain some style to change

--- js/components/Index.js
+++ js/components/Index.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View, PixelRatio, Image, Button, Alert} from 'react-n
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Message} from './Templates';
 
-class Index extends Component {
+
+export default class Index extends Component {
   static navigationOptions = {
     title: 'TIMEMORE Labs',
   };
@@ -11,9 +12,13 @@ class Index extends Component {
   render() {
     return (
       <View style={styles.container}>
-        {/*<Message/>*/}
+        <Message/>
         <Reader/>
-        <Setting/>
+        <Details/>
+        <View style={{flexDirection: 'row',justifyContent: 'center',alignItems:'center'}}>
+          <Image style={styles.settingImg} source={require('../../images/index_btn_setting.jpg')} />
+          <Text style={styles.settingContent} onPress={() => this.props.navigation.navigate('Details')}>设置参数</Text>
+        </View>
         <View>
           <Button title="开始冲煮" color='#6A6A6A' onPress={() => {Alert.alert('pressed');}} />
         </View>
@@ -26,62 +31,57 @@ class Index extends Component {
 class Reader extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '#dcdcdc', height: 567}}>
-        <Image source={require('../../images/cover.png')} style={{aspectRatio: 1.5, resizeMode: 'contain'}} />
-        <View style={{flex: 1, flexDirection: 'column', paddingTop: 64}}>
+      <View style={{flex:2, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '#dcdcdc', alignItems: 'center'}}>
+        <Image source={require('../../images/cover.png')} style={styles.coverImg} />
+        <View style={styles.flexColumn}>
           <Text style={styles.readerTitle}>咖啡萃取量(g)</Text>
           <Text style={styles.reader}>200</Text>
           <Text style={styles.readerTitle}>注水总量(g)</Text>
           <Text style={styles.reader}>200</Text>
-          {/*<Text style={styles.btnClear} onPress={() => {Alert.alert('pressed');}}>归零</Text>*/}
-          <Button title="归零" color='#6A6A6A' onPress={() => {Alert.alert('pressed');}} />
-
-        </View>
-      </View>
-    );
-  }
-}
-
-class Setting extends Component {
-  render() {
-    return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <SingleSetting name='咖啡豆' value='曼特宁' img={require('../../images/icon_brand.png')}/>
-
-        <View style={styles.flexrow}>
-          <View style={styles.flexrow}>
-            <SingleSetting name='粉重' value='20g' img={require('../../images/icon_beanweight.png')}/>
-            <Text>读秤</Text>
+          <View style={styles.btnClear}>
+            <Text style={styles.btnClearText} onPress={() => {Alert.alert('pressed');}}>归零</Text>
           </View>
-          <SingleSetting name='萃取量' value='240g' img={require('../../images/icon_waterweight.png')}/>
-        </View>
+          {/*<Button title="归零" color='#6A6A6A' onPress={() => {Alert.alert('pressed');}} />*/}
 
-        <View style={styles.flexrow}>
-          <SingleSetting name='粉液比' value='1:12' img={require('../../images/icon_proportion.png')}/>
-          <SingleSetting name='温度' value='92℃' img={require('../../images/icon_temp.png')}/>
-        </View>
-
-        <View style={styles.flexrow}>
-          <SingleSetting name='研磨度' value='3.5' img={require('../../images/icon_grandsize.png')}/>
-          <SingleSetting name='预计时间' value='02:30' img={require('../../images/icon_time.png')}/>
-        </View>
-
-        <View style={styles.flexrow}>
-          <Image style={styles.settingImg} source={require('../../images/index_btn_setting.jpg')} />
-          <Text style={styles.settingContent}>设置参数</Text>
         </View>
       </View>
     );
   }
 }
 
-class SingleSetting extends Component {
+export class Details extends Component {
   render() {
     return (
-      <View style={[styles.flexrow,styles.fullwidth]}>
+      <View style={{flex: 1,flexDirection: 'column',}}>
+        <SingleDetail name='咖啡豆' value='曼特宁' img={require('../../images/icon_brand.png')}/>
+
+        <View style={styles.flexRow}>
+          <SingleDetail name='粉重' value='20g' img={require('../../images/icon_beanweight.png')} text='读秤'/>
+          <SingleDetail name='萃取量' value='240g' img={require('../../images/icon_waterweight.png')}/>
+        </View>
+
+        <View style={styles.flexRow}>
+          <SingleDetail name='粉液比' value='1:12' img={require('../../images/icon_proportion.png')}/>
+          <SingleDetail name='温度' value='92℃' img={require('../../images/icon_temp.png')}/>
+        </View>
+
+        <View style={styles.flexRow}>
+          <SingleDetail name='研磨度' value='3.5' img={require('../../images/icon_grandsize.png')}/>
+          <SingleDetail name='预计时间' value='02:30' img={require('../../images/icon_time.png')}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+class SingleDetail extends Component {
+  render() {
+    return (
+      <View style={styles.flexRow}>
         <Image style={styles.settingIcon} source={this.props.img} />
         <Text style={styles.settingName}>{this.props.name}</Text>
         <Text style={styles.settingValue}>{this.props.value}</Text>
+        <Text style={{marginLeft:20, fontSize:26, color:'#53B2F0'}}>{this.props.text}</Text>
       </View>
     );
   }
@@ -91,19 +91,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  flexrow: {
+  flexRow: {
     flex: 1,
     flexDirection:'row',
-    alignItems: 'center',
+    alignItems: 'center'
   },
-  fullwidth: {
-    width: 750,
+  flexColumn: {
+    flex: 1,
+    flexDirection:'column',
   },
-  halfwidth: {
-    width: 375,
+  coverImg: {
+    resizeMode: 'contain',
+    marginTop: 60,
+    marginBottom: 60,
+    marginLeft: 25,
+    marginRight: 25,
+    flex: 1,
   },
   readerTitle: {
     color: '#5B5B5B',
@@ -127,6 +131,8 @@ const styles = StyleSheet.create({
     marginTop: 37,
     width: 102,
     height: 102,
+  },
+  btnClearText: {
     color: '#BF9253',
     fontSize: 30,
   },
@@ -135,7 +141,6 @@ const styles = StyleSheet.create({
     height: 44,
     marginTop:10,
     marginLeft:40,
-
   },
   settingName: {
     color: '#5B5B5B',
@@ -161,11 +166,5 @@ const styles = StyleSheet.create({
     fontSize:30,
     color:'#53B2F0',
   },
-  btnStart: {
-     fontSize:40,
-  },
 
 });
-
-export default Index
-
--- js/components/Templates.js
+++ js/components/Templates.js
@@ -5,7 +5,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 export class Message extends Component {
   render() {
     return (
-      <View style={[styles.flexrow, styles.messageContainer]}>
+      <View style={styles.messageContainer}>
         <Ionicons name="ios-alert" size={23} color='tomato'/>
         <Text style={styles.message}>当前蓝牙不可用，请检查你的蓝牙设置</Text>
       </View>
@@ -14,12 +14,12 @@ export class Message extends Component {
 }
 
 const styles = StyleSheet.create({
-  flexrow: {
-    flex: 1,
+  messageContainer: {
+    height: 100,
+    flex: 0.3,
     flexDirection:'row',
     alignItems: 'center',
-  },
-  messageContainer: {
+    justifyContent: 'center',
     backgroundColor: '#ffdfe0',
   },
   message: {
