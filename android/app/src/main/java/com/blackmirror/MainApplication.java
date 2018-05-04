ae48d663be4d413323150f6f5afa250ab7abd6ef
dong bin jiang <jiang.db@gmail.com>
2018-05-04 21:55:35 +0800

    Remove react-native sag and react-native highcharts

--- android/app/src/main/java/com/blackmirror/MainApplication.java
+++ android/app/src/main/java/com/blackmirror/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.github.wuxudong.rncharts.MPAndroidChartPackage;
-import com.horcrux.svg.SvgPackage;
 import com.polidea.reactnativeble.BlePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new MPAndroidChartPackage(),
-            new SvgPackage(),
             new BlePackage()
       );
     }
