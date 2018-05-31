00eecbc0ee3e2f08749b4bff01aeafa4a6482758
ya_nan <ya_nan@ivideocloud.com>
2018-05-31 11:03:27 +0800

    add webserver reducer

---
+++ js/reducers/webServer.js
@@ -0,0 +1,26 @@
+const initialState = {
+  token: null,
+  expireAt: null,
+  refreshExpireAt: null
+}
+ 
+import type { Action } from "../actions/types";
+
+function webServer(state, action) {
+  if (typeof state === 'undefined') {
+    return initialState
+  }
+ 
+  switch (action.type) {
+    case "WEB_SERVER_STATE_CHANGE":
+      return Object.assign({}, state, action.state)
+
+    case "WECHAT_LOGOUT":
+      return Object.assign({}, state, initialState)
+
+    default:
+      return state;
+  }
+}
+
+module.exports = webServer;
