be6ab3f7bf75640ba968fdfd7f6dc77334c3efb4
ya_nan <ya_nan@ivideocloud.com>
2018-07-03 18:16:54 +0800

    add comment and remove java package

--- android/app/src/main/java/com/rncharts/charts/LineChartManager.java
+++ android/app/src/main/java/com/rncharts/charts/LineChartManager.java
@@ -1,26 +1,12 @@
 package com.rncharts.charts;
 
-
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.SimpleViewManager;
-import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableType;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.infer.annotation.Assertions;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.common.MapBuilder;
 import java.util.Map;
-import java.util.List;
-
 import javax.annotation.Nullable;
-import android.util.Log;
-
-
-import android.app.Activity;
-import java.lang.Boolean;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
@@ -59,7 +45,6 @@ public class LineChartManager extends BarLineChartBaseManager<LineChart, Entry> 
 
     @Override
     public Map<String,Integer> getCommandsMap() {
-        Log.d("ReactNativeJS"," View manager getCommandsMap:");
         return MapBuilder.of(
             "addEntry",COMMAND_ADD_ENTRY,
             "updateEntry",COMMAND_UPDATE_ENTRY,
@@ -94,20 +79,22 @@ public class LineChartManager extends BarLineChartBaseManager<LineChart, Entry> 
 
         switch (commandType) {
             case COMMAND_ADD_ENTRY:
+            //Add the given Entry to the DataSet at the specified dataset index
                 lineData.addEntry(createEntry(args, 1),args.getInt(0));
                 return;
             case COMMAND_UPDATE_ENTRY:
+            //Add the given Entry to the DataSet at the specified dataset index and remove the first Entry of the same DataSet
                 lineData.addEntry(createEntry(args, 1),args.getInt(0));
                 lineData.removeEntry(0,args.getInt(0));
                 return;
             case COMMAND_REFRESH:
-                Log.d("ReactNativeJS", "receiveCommand: COMMAND_REFRESH");
-
+            //let the chart know it's data changed and refresh chart
                 lineData.notifyDataChanged(); // let the data know a dataSet changed
                 lineChart.notifyDataSetChanged(); // let the chart know it's data changed
                 lineChart.invalidate(); // refresh
                 return;
             case COMMAND_REMOVE_DATASET:
+            //Remove the DataSet at the given index from the ChartData object
                 lineData.removeDataSet(args.getInt(0));
                 lineChart.notifyDataSetChanged(); // let the chart know it's data changed
                 lineChart.invalidate(); // refresh
